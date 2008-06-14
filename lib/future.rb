b0c8ff28f4cec214e5c84d4a2ef832e89ad1b1b7
cho45 <cho45@lowreal.net>
2008-06-14 22:18:56 +0900

    Implemented

--- lib/future.rb
+++ lib/future.rb
@@ -1,4 +1,46 @@
 
 class Future
+	def initialize(obj, name, args, block)
+		@obj, @name, @args, @block = obj, name, args, block
+		@th  = Thread.start do
+			Thread.pass # 一応明示的に pass しておく
+			@obj.send(@name, *@args, &@block)
+		end
+	end
 
+	def method_missing(name, *args, &block)
+		@th.value.send(name, *args, &block)
+	end
+
+	# 全部委譲してなりすます
+	Object.instance_methods.each do |m|
+		next if %w|__send__ __id__ object_id|.include? m.to_s
+		undef_method m
+	end
+
+	module ObjectExtension
+		def async(name=nil, *args, &block)
+			name ? Future.new(self, name, args, block) : AsyncObject.new(self)
+		end
+
+		class AsyncObject
+			def initialize(obj)
+				@obj = obj
+			end
+
+			def method_missing(name, *args, &block)
+				Future.new(@obj, name, args, block)
+			end
+
+			Object.instance_methods.each do |m|
+				next if %w|__send__ __id__ object_id|.include? m.to_s
+				undef_method m
+			end
+		end
+	end
 end
+
+class Object
+	include Future::ObjectExtension
+end
+
--- spec/future_spec.rb
+++ spec/future_spec.rb
@@ -5,11 +5,78 @@ $LOAD_PATH << "lib"
 $LOAD_PATH << "../lib"
 
 require "future"
-include Net::IRC
-include Constants
+
+require "rubygems"
+require "spec"
+
+require "timeout"
+
+class TestObject
+	@@result = []
+
+	def result
+		ret = @@result.dup
+		@@result.clear
+		ret
+	end
+
+	def heavy(n, value, &block)
+		sleep n
+		@@result << [n, value]
+		if block_given?
+			yield
+		else
+			value
+		end
+	end
+end
 
 describe Future do
-	it "" do
+	before do
+		@o = TestObject.new
+	end
+
+	it "should handle message asynchronously" do
+		future = nil
+
+		timeout(0.1) do
+			future = Future.new(Kernel, :sleep, [ 0.5 ], nil)
+		end
+
+		Proc.new {
+			timeout(0.1) do
+				future.inspect
+			end
+		}.should raise_error(Timeout::Error)
+
+		future.inspect # block
+
+		Proc.new {
+			timeout(0.1) do
+				future.inspect
+			end
+		}.should_not raise_error(Timeout::Error)
+	end
+
+	it "should append async mehtod to Object" do
+		# normal
+		@o.heavy(0.1, :foo)
+		@o.result.should == [ [0.1, :foo] ]
+
+		# async
+		future = @o.async(:heavy, 0.1, :foo)
+		@o.result.should == [ ]
+		future.inspect.should == ":foo"
+		@o.result.should == [ [0.1, :foo] ]
+	end
+
+	it "should handle block correctly" do
+		future = @o.async(:heavy, 0.1, :foo) {
+			:bar
+		}
+		@o.result.should == [ ]
+		future.inspect.should == ":bar"
+		@o.result.should == [ [0.1, :foo] ]
 	end
 end
 
