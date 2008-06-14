650b2b901b3efbbc2295841aca37d00eea83fe3e
cho45 <cho45@lowreal.net>
2008-06-14 21:58:27 +0900

    init

---
+++ lib/future.rb
@@ -0,0 +1,4 @@
+
+class Future
+
+end
---
+++ spec/future_spec.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+
+$LOAD_PATH << "lib"
+$LOAD_PATH << "../lib"
+
+require "future"
+include Net::IRC
+include Constants
+
+describe Future do
+	it "" do
+	end
+end
+
